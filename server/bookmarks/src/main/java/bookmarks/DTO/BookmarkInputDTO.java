279c4201b82fa14c28290c1e36643169db626fc8
rus-77 <casp.ricci@gmail.com>
2023-03-31 20:23:24 +0300

    Create BookmarkInputDTO

---
+++ server/bookmarks/src/main/java/bookmarks/DTO/BookmarkInputDTO.java
@@ -0,0 +1,11 @@
+package bookmarks.DTO;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class BookmarkInputDTO {
+    private String link;
+    private String collection;
+}
