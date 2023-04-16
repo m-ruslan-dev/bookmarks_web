41a961b1f5b9c47bd4dd0c5574533c1b683116ee
rus-77 <casp.ricci@gmail.com>
2023-04-16 20:54:03 +0300

    Add roles entity

--- server/bookmarks/src/main/java/bookmarks/models/Role.java
+++ server/bookmarks/src/main/java/bookmarks/models/Role.java
@@ -1,41 +1,26 @@
 package bookmarks.models;
 
-import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotEmpty;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.ArrayList;
-import java.util.List;
-
 @Entity
-@Table(name = "user")
+@Table(name = "role")
 @Getter
 @Setter
-public class User {
+public class Role {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @NotEmpty
-    private Long id;
+    Long id;
 
     @Column(unique = true)
     @NotEmpty
-    private String username;
-
-    @NotEmpty
-    private String password;
-
-    @NotEmpty
-    private String email;
-
-    @OneToMany(mappedBy = "user", cascade = CascadeType.PERSIST, orphanRemoval = true)
-    @NotEmpty
-    private List<Bookmark> bookmarks = new ArrayList<Bookmark>();
+    String role;
 }
