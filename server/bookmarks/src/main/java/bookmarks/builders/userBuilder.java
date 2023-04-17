27697075e7fc2066ebba92d8fe42a02b443dfd20
rus-77 <casp.ricci@gmail.com>
2023-04-17 21:40:19 +0300

    Add role assignment to UserBuilder

--- server/bookmarks/src/main/java/bookmarks/builders/userBuilder.java
+++ server/bookmarks/src/main/java/bookmarks/builders/userBuilder.java
@@ -4,19 +4,21 @@ import org.springframework.stereotype.Component;
 
 import bookmarks.exceptions.EmptyFieldsException;
 import bookmarks.interfaces.IUserBuilder;
+import bookmarks.models.Role;
 import bookmarks.models.User;
 
 @Component
 public class UserBuilder implements IUserBuilder {
     private User user = new User();
 
-    public User buildUser(String username, String password, String email) throws EmptyFieldsException {
-        if (username == null || password == null || email == null) {
+    public User buildUser(String username, String password, String email, Role role) throws EmptyFieldsException {
+        if (username == null || password == null || email == null || role == null) {
             throw new EmptyFieldsException("Cannot create a user with empty fields");
         } else {
             user.setUsername(username);
             user.setPassword(password);
             user.setEmail(email);
+            user.getRoles().add(role);
             return user;
         }
     }
--- server/bookmarks/src/main/java/bookmarks/interfaces/IUserBuilder.java
+++ server/bookmarks/src/main/java/bookmarks/interfaces/IUserBuilder.java
@@ -1,8 +1,9 @@
 package bookmarks.interfaces;
 
 import bookmarks.exceptions.EmptyFieldsException;
+import bookmarks.models.Role;
 import bookmarks.models.User;
 
 public interface IUserBuilder {
-    public User buildUser(String username, String password, String email) throws EmptyFieldsException;
+    public User buildUser(String username, String password, String email, Role role) throws EmptyFieldsException;
 }
