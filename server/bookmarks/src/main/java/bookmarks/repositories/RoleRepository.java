d01a535a335003144b893b73b4fb5469645aa5df
rus-77 <casp.ricci@gmail.com>
2023-04-17 21:31:49 +0300

    Add role service

--- server/bookmarks/src/main/java/bookmarks/repositories/RoleRepository.java
+++ server/bookmarks/src/main/java/bookmarks/repositories/RoleRepository.java
@@ -1,9 +1,16 @@
 package bookmarks.repositories;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import bookmarks.models.Role;
 
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
+    @Query("""
+            SELECT r FROM Role r WHERE r.role = :role
+            """)
+    Optional<Role> findRoleByName(String role);
 }
---
+++ server/bookmarks/src/main/java/bookmarks/services/RoleService.java
@@ -0,0 +1,21 @@
+package bookmarks.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import bookmarks.exceptions.RoleNotFoundException;
+import bookmarks.models.Role;
+import bookmarks.repositories.RoleRepository;
+
+@Service
+public class RoleService {
+    @Autowired
+    RoleRepository roleRepository;
+
+    public Role getRoleByName(String name) throws RoleNotFoundException {
+        final Optional<Role> role = roleRepository.findRoleByName(name);
+        return role.orElseThrow(() -> new RoleNotFoundException("Could not find role: " + name));
+    }
+}
