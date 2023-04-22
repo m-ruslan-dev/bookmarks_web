6cb5de9e2e843af5a872123d410945086640cac5
rus-77 <casp.ricci@gmail.com>
2023-04-22 22:02:43 +0300

    Refactor builder classes

--- server/bookmarks/src/main/java/bookmarks/builders/bookmarkBuilder.java
+++ server/bookmarks/src/main/java/bookmarks/builders/bookmarkBuilder.java
@@ -10,12 +10,12 @@ import bookmarks.models.User;
 
 @Component
 public class BookmarkBuilder implements IBookmarkBuilder {
-    Bookmark bookmark = new Bookmark();
 
     public Bookmark buildBookmark(User user, String link, String collection) throws EmptyFieldsException {
         if (user == null || link == null || collection == null) {
             throw new EmptyFieldsException("Cannot create bookmark with empty fields.");
         } else {
+            Bookmark bookmark = new Bookmark();
             bookmark.setUser(user);
             bookmark.setLink(link);
             bookmark.setCollection(collection);
--- server/bookmarks/src/main/java/bookmarks/builders/userBuilder.java
+++ server/bookmarks/src/main/java/bookmarks/builders/userBuilder.java
@@ -9,12 +9,12 @@ import bookmarks.models.User;
 
 @Component
 public class UserBuilder implements IUserBuilder {
-    private User user = new User();
 
     public User buildUser(String username, String password, String email, Role role) throws EmptyFieldsException {
         if (username == null || password == null || email == null || role == null) {
             throw new EmptyFieldsException("Cannot create a user with empty fields");
         } else {
+            User user = new User();
             user.setUsername(username);
             user.setPassword(password);
             user.setEmail(email);
