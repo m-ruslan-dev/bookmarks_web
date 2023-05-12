ff4bdb201bd0ad94e8bf907c1d270e872c56d9c2
rus-77 <casp.ricci@gmail.com>
2023-05-12 22:12:31 +0300

    Add basic handler for new bookmark form

--- client/src/components/AddBookmarkModal/AddBookmarkModal.js
+++ client/src/components/AddBookmarkModal/AddBookmarkModal.js
@@ -1,20 +1,42 @@
 import React from "react";
+import useCsrfToken from "../../hooks/useCsrfToken";
+
+import { newBookmarkHandler } from "./newBookmarkHandler";
 
 const AddBookmarkModal = ({ setModalOpen }) => {
+  const csrfToken = useCsrfToken();
   return (
     <div className="modal">
       <div className="modal__overlay"></div>
-      <form className="modal__container">
+      <form
+        className="modal__container"
+        onSubmit={(event) => newBookmarkHandler(event, csrfToken)}
+      >
         <h2 className="modal__heading">New Bookmark</h2>
 
-        {/* Input link */}
-        <label className="label-text" for="bookmark_link">
+        {/* Inputs */}
+        <label className="label-text" htmlFor="bookmark_title">
+          Title
+        </label>
+        <input
+          type="text"
+          id="bookmark_title"
+          className="input-text"
+          name="bookmark_title"
+        ></input>
+
+        <label className="label-text" htmlFor="bookmark_link">
           Link
         </label>
-        <input type="text" id="bookmark_link" className="input-text"></input>
+        <input
+          type="text"
+          id="bookmark_link"
+          className="input-text"
+          name="bookmark_link"
+        ></input>
 
         {/* Select collection */}
-        <label className="label-text" for="choose_collection">
+        <label className="label-text" htmlFor="choose_collection">
           Collection
         </label>
         <select id="choose_collection" placeholder="Select...">
@@ -26,19 +48,24 @@ const AddBookmarkModal = ({ setModalOpen }) => {
         </select>
 
         {/* Create new collection */}
-        <label className="label-text" for="new_collection">
+        <label className="label-text" htmlFor="new_collection">
           Create collection
         </label>
-        <input type="text" id="new_collection" className="input-text"></input>
+        <input
+          type="text"
+          id="new_collection"
+          className="input-text"
+          name="new_collection"
+        ></input>
 
         {/* Buttons */}
         <div className="modal__buttons">
           <button
             type="button"
-            className="modal__cancel cancel-btn"
+            className="modal__close close-btn"
             onClick={() => setModalOpen(false)}
           >
-            Cancel
+            Close
           </button>
           <button type="submit" className="modal__submit submit-btn">
             Submit
---
+++ client/src/components/AddBookmarkModal/newBookmarkHandler.js
@@ -0,0 +1,29 @@
+export const newBookmarkHandler = async (event, csrfToken) => {
+  event.preventDefault();
+
+  // Get input values
+  const formData = new FormData(event.target);
+  const title = formData.get("bookmark_title");
+  const link = formData.get("bookmark_link");
+  const collection = formData.get("new_collection");
+
+  const bookmarkData = {
+    title,
+    link,
+    collection,
+  };
+  // Send bookmark data
+  try {
+    await fetch("http://localhost:8080/bookmarks", {
+      method: "POST",
+      credentials: "include",
+      headers: {
+        "Content-Type": "application/json",
+        "X-XSRF-TOKEN": csrfToken,
+      },
+      body: JSON.stringify(bookmarkData),
+    });
+  } catch (error) {
+    console.log(error);
+  }
+};
