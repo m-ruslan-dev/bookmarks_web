3195b50e317cc6019ba15bba3ec690e1f5eac47d
rus-77 <casp.ricci@gmail.com>
2023-05-14 22:26:56 +0300

    Add Dashboard route protection

--- client/src/App.js
+++ client/src/App.js
@@ -4,13 +4,21 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.scss";
 import Dashboard from "./pages/Dashboard/Dashboard";
 import Authentication from "./pages/Authentication/Authentication";
+import WithAuthentication from "./HOC/WithAuthentication/WithAuthentication";
 
 let App = () => {
   return (
     <BrowserRouter>
       <>
         <Routes>
-          <Route path="/" element={<Dashboard />} />
+          <Route
+            path="/"
+            element={
+              <WithAuthentication>
+                <Dashboard />
+              </WithAuthentication>
+            }
+          />
           <Route path="/auth/:formType" element={<Authentication />} />
         </Routes>
       </>
