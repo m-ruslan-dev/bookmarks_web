acfffd520001c3d7cbbe016cec1ee571fc06fc1c
Russ <casp.ricci@gmail.com>
2025-03-21 20:21:40 +0200

    Refactor: Restructure bookmark card layout
    - Moved title and action buttons into a "header" div.
    - Added description below the header.
    - Positioned URL and creation date at the bottom.

--- client/src/components/Card/Card.js
+++ client/src/components/Card/Card.js
@@ -8,9 +8,9 @@ import handleDeleteBookmark from "./handleDeleteBookmark";
 // Context
 import { BookmarksContext } from "../../contexts/BookmarksContext";
 // Icon
-import { RiDeleteBin6Line } from "react-icons/ri";
+import { RiDeleteBin6Line} from "react-icons/ri";
 
-const Card = ({ title, link, bookmarkId, creationDate }) => {
+const Card = ({ title, link, bookmarkId, description, creationDate }) => {
   const csrfToken = useCsrfToken();
   // Get creation date for the provided bookmark
   const { year, month, date } = extractYearMonthDate(creationDate);
@@ -20,21 +20,34 @@ const Card = ({ title, link, bookmarkId, creationDate }) => {
   return (
     <div className="card">
       <div className="card__body">
-        <p className="card__date">{`${date} ${month} ${year}`}</p>
-        <h4 className="card__title">{title}</h4>
+        <div className="card__header">
+          <h4 className="card__title">{title}</h4>
+          <div className="card__buttons">
+            <button
+              type="button"
+              className="card__delete"
+              onClick={(event) =>
+                handleDeleteBookmark(
+                  event,
+                  bookmarkId,
+                  csrfToken,
+                  refreshBookmarks
+                )
+              }
+            >
+              <RiDeleteBin6Line />
+            </button>
+          </div>
+        </div>
+
+        <p className="card__description">
+          {description}
+        </p>
         <a href={link} className="card__url">
           {link}
         </a>
+        <p className="card__date">{`${date} ${month} ${year}`}</p>
       </div>
-      <button
-        type="button"
-        className="card__delete"
-        onClick={(event) =>
-          handleDeleteBookmark(event, bookmarkId, csrfToken, refreshBookmarks)
-        }
-      >
-        <RiDeleteBin6Line />
-      </button>
     </div>
   );
 };
